de249fd0c14aa994fbd08169f25e757534cc7714
derEskimo <hannes.zakes@gmx.de>
2019-02-01 10:58:12 +0100

    Add Style and First Layout for cloud.php

--- cloud.php
+++ cloud.php
@@ -1,11 +1,31 @@
-<?php
-session_start();
-if(!isset($_SESSION['userid'])) {
-    die('Bitte zuerst <a href="login.php">einloggen</a>');
-}
+<!DOCTYPE html>
+<html>
+<head>
+    <title>baustellenCloud</title>
+    <link rel="shortcut icon" type="image/x-icon" href="media/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="cloud.css">
+</head>
+<body>
 
-//Abfrage der Nutzer ID vom Login
-$userid = $_SESSION['userid'];
+<header>
+    <h2>Cities</h2>
+</header>
 
-echo "Hallo User: ".$userid;
-?>
+<section>
+    <nav>
+        <ul>
+            <li><a href="#">London</a></li>
+            <li><a href="#">Paris</a></li>
+            <li><a href="#">Tokyo</a></li>
+        </ul>
+    </nav>
+
+    <article>
+        <h1>London</h1>
+        <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
+        <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+    </article>
+</section>
+
+
+</body>
