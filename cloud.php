4120b5466035dfbe11d421d8d2f4c3d9d1c3dbb4
derEskimo <hannes.zakes@gmx.de>
2019-02-01 13:19:04 +0100

    Add Baustellen-Scrollbar to left

--- cloud.php
+++ cloud.php
@@ -1,28 +1,66 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>baustellenCloud</title>
     <link rel="shortcut icon" type="image/x-icon" href="media/favicon.ico">
     <link rel="stylesheet" type="text/css" href="cloud.css">
 </head>
-<body>
-   <left>
-        <ul>
-            <li><a href="#">London</a></li>
-            <li><a href="#">Paris</a></li>
-            <li><a href="#">Tokyo</a></li>
-        </ul>
-    </left>
 
-    <right>
+<body>
+
+<left>
+    <div class="baustellenSelect">
+        <select size="100">
+                <option>Baustelle 1</option>
+                <option>Baustelle 2</option>
+                <option>Baustelle 3</option>
+                <option>Baustelle 1</option>
+                <option>Baustelle 2</option>
+                <option>Baustelle 3</option>
+                <option>Baustelle 1</option>
+                <option>Baustelle 2</option>
+                <option>Baustelle 3</option>
+                <option>Baustelle 1</option>
+                <option>Baustelle 2</option>
+                <option>Baustelle 3</option>
+                <option>Baustelle 1</option>
+                <option>Baustelle 2</option>
+                <option>Baustelle 3</option>
+                <option>Baustelle 1</option>
+                <option>Baustelle 2</option>
+                <option>Baustelle 3</option>
+                <option>Baustelle 1asdaaaaaaaaadsasdsdaasdadssdadsd</option>
+                <option>Baustelle 2</option>
+                <option>Baustelle 3</option>
+                <option>Baustelle 1</option>
+                <option>Baustelle 2</option>
+                <option>Baustelle 3</option>
+                <option>Baustelle 1</option>
+                <option>Baustelle 2</option>
+                <option>Baustelle 3</option>
+                <option>Baustelle 1</option>
+                <option>Baustelle 2</option>
+                <option>Baustelle 3</option>
+                <option>Baustelle 1</option>
+                <option>Baustelle 2</option>
+                <option>Baustelle 3</option>
+            </select>
+    </div>
+</left>
+
+<right>
     <menue>
         <h2>Cities</h2>
     </menue>
 
     <content>
         <h1>London</h1>
-        <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
-        <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+        <p>London is the capital city of England. It is the most populous city in the United Kingdom, with a
+            metropolitan area of over 13 million inhabitants.</p>
+        <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to
+            its founding by the Romans, who named it Londinium.</p>
     </content>
-    </right>
+</right>
+
 </body>
